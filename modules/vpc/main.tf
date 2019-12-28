c2303a46a0c8157bf04f8d858112b68082cf9541
rbhokare145 <rbhokare145@gmail.com>
2019-12-28 16:48:31 +0530

    updating workable VPC configs

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,9 +1,10 @@
 resource "aws_vpc" "kubeVpc" {
-  cidr_block = "${cidr_range}"
-  enable_dns_hostnames =  "true"
+  cidr_block = "${var.cidr_range}"
+  enable_dns_hostnames = "true"
   enable_dns_support = "true"
 
-  tags {
+
+  tags = {
 
     Name = "kubeNetVpc"
 
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,4 +1,4 @@
-variable "cidr_output" {
+output "cidr_output" {
   value = aws_vpc.kubeVpc.cidr_block
   description = "configured CIDR range"
 }
