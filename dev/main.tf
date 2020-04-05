0140d1ceb93a058a56363708324d0ab9b5605cc9
rbhokare145 <rbhokare145@gmail.com>
2020-04-05 22:13:30 +0530

    update styling with terraform fmt

--- dev/main.tf
+++ dev/main.tf
@@ -4,106 +4,106 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-      bucket = "kube-terraform-up-and-running-locks"
-      key = "global/s3/terraform.tfstate"
-      region = "ap-south-1"
-      dynamodb_table = "terraform-up-and-running-locks"
-      encrypt = "true"
+    bucket         = "kube-terraform-up-and-running-locks"
+    key            = "global/s3/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = "true"
   }
 }
 
 module "kubeS3" {
-  source = "../modules/s3"
-  bucket_name = "${var.bucket_name}"
+  source                 = "github.com/rbhokare145/terraform_aws//modules/s3?ref=v2.0.0"
+  bucket_name            = "${var.bucket_name}"
   bucket_versioning_mode = "${var.versioning_mode}"
 }
 
 module "kubeDynamoDB" {
-  source = "../modules/dynamodb"
+  source        = "github.com/rbhokare145/terraform_aws//modules/dynamodb?ref=v2.0.0"
   dynamodb_name = "${var.dynamodb_name}"
 }
 
 
 module "kubeVpc" {
-  source = "github.com/rbhokare145/terraform_aws//modules/vpc?ref=v1.0.0"
+  source     = "github.com/rbhokare145/terraform_aws//modules/vpc?ref=v2.0.0"
   cidr_range = "${var.user_cidr_range}"
 }
 
 module "kubeSubnet" {
-  source = "../modules/subnet"
+  source          = "github.com/rbhokare145/terraform_aws//modules/subnet?ref=v2.0.0"
   pub1_cidr_range = "${var.user_pub1_cidr_range}"
   pub2_cidr_range = "${var.user_pub2_cidr_range}"
   pri1_cidr_range = "${var.user_pri1_cidr_range}"
   pri2_cidr_range = "${var.user_pri2_cidr_range}"
-  vpc_id = "${module.kubeVpc.kubeVpc_id}"
+  vpc_id          = "${module.kubeVpc.kubeVpc_id}"
 
 }
 
 module "kubeInternetGateway" {
-  source = "github.com/rbhokare145/terraform_aws//modules/internetGateway?ref=v1.0.0"
+  source = "github.com/rbhokare145/terraform_aws//modules/internetGateway?ref=v2.0.0"
   vpc_id = "${module.kubeVpc.kubeVpc_id}"
 }
 
 module "kubeEip" {
-  source = "github.com/rbhokare145/terraform_aws//modules/eip?ref=v1.0.0"
+  source = "github.com/rbhokare145/terraform_aws//modules/eip?ref=v2.0.0"
 }
 
 
 module "kubeNatGateway" {
-  source = "github.com/rbhokare145/terraform_aws//modules/natgateway?ref=v1.0.0"
+  source           = "github.com/rbhokare145/terraform_aws//modules/natgateway?ref=v2.0.0"
   eip_allocationid = "${module.kubeEip.eip_allocationid}"
-  public_subnetid = "${module.kubeSubnet.publicSubnet1_id}"
+  public_subnetid  = "${module.kubeSubnet.publicSubnet1_id}"
 }
 
 module "kubeRouteTable" {
-  source = "github.com/rbhokare145/terraform_aws//modules/routetables?ref=v1.0.0"
-  vpc_id = "${module.kubeVpc.kubeVpc_id}"
-  private_sub1_id = "${module.kubeSubnet.privateSubnet1_id}"
-  private_sub2_id = "${module.kubeSubnet.privateSubnet2_id}"
+  source              = "github.com/rbhokare145/terraform_aws//modules/routetables?ref=v2.0.0"
+  vpc_id              = "${module.kubeVpc.kubeVpc_id}"
+  private_sub1_id     = "${module.kubeSubnet.privateSubnet1_id}"
+  private_sub2_id     = "${module.kubeSubnet.privateSubnet2_id}"
   private_routable_id = "${module.kubeRouteTable.private_routable_id}"
 
-  public_sub1_id = "${module.kubeSubnet.publicSubnet1_id}"
-  public_sub2_id = "${module.kubeSubnet.publicSubnet2_id}"
+  public_sub1_id     = "${module.kubeSubnet.publicSubnet1_id}"
+  public_sub2_id     = "${module.kubeSubnet.publicSubnet2_id}"
   public_routable_id = "${module.kubeRouteTable.public_routable_id}"
 
   nat_gw_id = "${module.kubeNatGateway.nat_id}"
-  igw_id = "${module.kubeInternetGateway.internetgateway_id}"
+  igw_id    = "${module.kubeInternetGateway.internetgateway_id}"
 }
 
 module "kubeNacl" {
-  source = "../modules/nacl"
-  vpc_id = "${module.kubeVpc.kubeVpc_id}"
-  cidr_range = "${var.user_cidr_range}"
+  source       = "github.com/rbhokare145/terraform_aws//modules/nacl?ref=v2.0.0"
+  vpc_id       = "${module.kubeVpc.kubeVpc_id}"
+  cidr_range   = "${var.user_cidr_range}"
   user_iprange = "${var.user_ip}"
 }
 
 module "kubeSecurityGroup" {
-  source = "../modules/securityGroup"
-  vpc_id = "${module.kubeVpc.kubeVpc_id}"
-  cidr_range = "${var.user_cidr_range}"
+  source       = "github.com/rbhokare145/terraform_aws//modules/securityGroup?ref=v2.0.0"
+  vpc_id       = "${module.kubeVpc.kubeVpc_id}"
+  cidr_range   = "${var.user_cidr_range}"
   user_iprange = "${var.user_ip}"
 }
 
 module "kubeNodeKeyPair" {
-  source = "../modules/keypair"
-  key_name = "${var.user_key_name}"
+  source     = "github.com/rbhokare145/terraform_aws//modules/keypair?ref=v2.0.0"
+  key_name   = "${var.user_key_name}"
   public_key = "${var.user_public_key}"
 }
 
 module "kubeEni" {
-  source = "../modules/eni"
-  private_subnet_id = "${module.kubeSubnet.privateSubnet2_id}"
-  security_groupId = "${module.kubeSecurityGroup.KubeSecurityGroupId}"
+  source              = "github.com/rbhokare145/terraform_aws//modules/eni?ref=v2.0.0"
+  private_subnet_id   = "${module.kubeSubnet.privateSubnet2_id}"
+  security_groupId    = "${module.kubeSecurityGroup.KubeSecurityGroupId}"
   kubemaster_instance = "${module.kubeEc2Instance.kubeMaster_instanceId}"
 }
 
 module "kubeEc2Instance" {
-  source = "../modules/ec2"
-  ami_id = "${var.user_ami_id}"
-  ec2_type = "${var.user_ec2_type}"
+  source            = "github.com/rbhokare145/terraform_aws//modules/ec2?ref=v2.0.0"
+  ami_id            = "${var.user_ami_id}"
+  ec2_type          = "${var.user_ec2_type}"
   availibility_zone = "${module.kubeSubnet.privateSubnet2_az}"
-  security_groupId = "${module.kubeSecurityGroup.KubeSecurityGroupId}"
-  key_name = "${module.kubeNodeKeyPair.key_name}"
+  security_groupId  = "${module.kubeSecurityGroup.KubeSecurityGroupId}"
+  key_name          = "${module.kubeNodeKeyPair.key_name}"
   private_subnet_id = "${module.kubeSubnet.privateSubnet2_id}"
-  public_subnet_id = "${module.kubeSubnet.publicSubnet2_id}"
+  public_subnet_id  = "${module.kubeSubnet.publicSubnet2_id}"
 }
--- dev/variables.tf
+++ dev/variables.tf
@@ -1,63 +1,63 @@
 variable "user_region" {
   description = "User defined AWS region"
-  type = string
-  default = "ap-south-1" #Mumbai
+  type        = string
+  default     = "ap-south-1" #Mumbai
 }
 
 variable "user_cidr_range" {
   description = "user define perticular cidr range specific to each environment"
-  type = string
-  default = "192.168.10.0/24"
+  type        = string
+  default     = "192.168.10.0/24"
 }
 
 variable "user_ip" {
   description = "user machine ip-address range to allow user to access ec2 instances externelly"
-  type = string
+  type        = string
 }
 
 variable "user_pub1_cidr_range" {
   description = "public1 cidr range for the subnet"
-  type = string
-  default = "192.168.10.0/28"
+  type        = string
+  default     = "192.168.10.0/28"
 }
 
 variable "user_pub2_cidr_range" {
   description = "public1 cidr range for the subnet"
-  type = string
-  default = "192.168.10.16/28"
+  type        = string
+  default     = "192.168.10.16/28"
 }
 
 variable "user_pri1_cidr_range" {
   description = "private1 cidr range for the subnet"
-  type = string
-  default = "192.168.10.32/28"
+  type        = string
+  default     = "192.168.10.32/28"
 }
 
 variable "user_pri2_cidr_range" {
   description = "private2 cidr range for the subnet"
-  type = string
-  default = "192.168.10.48/28"
+  type        = string
+  default     = "192.168.10.48/28"
 }
 
 
 variable "user_key_name" {
   description = "name of key pair used by ec2 instance"
-  default = "kubenodekey"
+  default     = "kubenodekey"
 }
 
 variable "user_public_key" {
   description = "public key value"
-  default = "a3ViZW5vZGVrZXkK"
+  default     = "a3ViZW5vZGVrZXkK"
 }
 
 variable "user_ami_id" {
   description = "ami id to be use by ec2 instance"
-  default = "ami-0a74bfeb190bd404f"
+  default     = "ami-0a74bfeb190bd404f"
 }
 
 variable "user_ec2_type" {
   description = "type of the ec2 instance"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "bucket_name" {
