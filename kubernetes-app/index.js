dd4479c7e10318ecf1489d99a5201317f7352623
rbhokare145 <rbhokare145@gmail.com>
2020-05-02 17:56:14 +0530

    updating index.js for node js

--- kubernetes-app/index.js
+++ kubernetes-app/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-const requestHandler = (request, response) ==> {
+const requestHandler = (request, response) => {
         console.log('Received request for URL: ' + request.url);
         response.writeHead(200);
         response.end('Hello World!');
@@ -9,7 +9,7 @@ const requestHandler = (request, response) ==> {
 
 const server = http.createServer(requestHandler);
 
-server.listen(port, (error) ==> {
+server.listen(port, (error) => {
         if (error) throw error;
         console.log(`Your app is running on port ${port}`);
 });
